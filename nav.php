14a96c1bf0156e2aa92910dca9261bc42e26ae29
Ana Stadelhofer <ana.stadelhofer@outlook.com>
2022-09-23 21:34:03 -0300

    nav

--- nav.php
+++ nav.php
@@ -1,6 +1,6 @@
 
 <nav class="main">
-    <ul calss="container">
+    <ul class="container">
         <li><img src="//picsum.photos/100/60"></li>
         <li><a href="index.php">INICIO</a></li>
         <li><a href="sobre.php">SOBRE</a></li>
